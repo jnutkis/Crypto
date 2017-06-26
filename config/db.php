de529be7d5d3128e25afb0941da01c8a4f4b3e5e
jnutkis <james.nutkis@gmail.com>
2017-06-26 13:55:35 +0000

    PDO

--- config/db.php
+++ config/db.php
@@ -3,20 +3,24 @@ $servername = "localhost";
 $username = "cryptoadmin";
 $password = "Nutkisnutkis1!";
 $dbname = 'cryptoDB';
+$charset = 'utf8';
 
-$link = new mysqli($servername, $username, $password, $dbname);
+$dsn = "mysql:host=$servername;dbname=$dbname;charset=$charset";
+$opt = [
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
 
+$pdo = new PDO($dsn,$username,$password,$opt);
 #echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 #echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
+$balances = $pdo->query('SELECT * FROM PEOPLE');
+while ($row = $balances->fetch()) {
+	echo $row['ID'] . " - " . $row['USERNAME'] . "\n";
 
-
+}
 #mysqli_close($link);
 ?>
